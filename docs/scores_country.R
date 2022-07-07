726192c74b23df71cbfb39f6c20f33051cf6f7c3
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2022-07-07 10:56:20 -0500

    Update scores_country.R

--- docs/scores_country.R
+++ docs/scores_country.R
@@ -11,14 +11,18 @@ f.plot.scores = function(x){
        N = as.data.frame(table(d[,p], d.m[,i]))
        names(N) = c("j", "i", "N")
 
-  #  Likelihoods
-       p1 = N$N[N$i == 1]/sum(N$N[N$i == 1]) #  P(S_j | R_i)
-       p2 = N$N[N$i == 0]/sum(N$N[N$i == 0]) #  P(S_j | R_noi)
+  #  Likelihoods S_x
+       p1 = N$N[N$i == 1]/sum(N$N[N$i == 1]) #  P(S_x | R_i)
+       p2 = N$N[N$i == 0]/sum(N$N[N$i == 0]) #  P(S_x | R_noi)
 
+  # Priors
+  prior1 = sum(N$N[N$i == 1]) / sum(N$N) # P(R_i)
+  prior2 = sum(N$N[N$i == 0]) / sum(N$N) # P(R_noi)
+  
 scores = as.data.frame(table(d[,p], d.m[,i]))
 scores$p1 = round(p1, 3)
 scores$p2 = round(p2, 3)
-scores$score = log((p1+0.0001)/(p2+0.0005))
+scores$score = log((p1+0.0001)/(p2+0.0005)) + log((prior1+0.0001)/(prior2+0.0005))
 
 score=scores$score
 names(score) = scores$Var1
